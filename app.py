3034e51a7bb81a5defd83d47e38df460ef360d2e
ayoubensalem <ayoubensalem@gmail.com>
2017-12-24 16:32:49 +0000

    Replacing CrawlerProcess with CrawlerRunner

--- app.py
+++ app.py
@@ -5,17 +5,25 @@ from flask_restful import Resource, Api
 from news import NewsSpider
 from galerie import GalerieSpider
 import json
+import scrapy.crawler as crawler
+from multiprocessing import Process, Queue
+from twisted.internet import reactor
+
 
 app = Flask(__name__)
 api = Api(app)
 
 class News(Resource):
     def get(self):
-        process = CrawlerProcess({
-            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-        })
-        process.crawl(NewsSpider)
-        process.start()
+        runner = CrawlerRunner()
+        d = runner.crawl(NewsSpider)
+        d.addBoth(lambda _: reactor.stop())
+        reactor.run()
+        # process = CrawlerProcess({
+        #     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        # })
+        # process.crawl(NewsSpider)
+        # process.start()
         return {'news': NewsSpider.getNews() }
 
 
