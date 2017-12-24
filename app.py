a39fd22dc138d1567854950cd9ce38c9d1b35b05
ayoubensalem <ayoubensalem@gmail.com>
2017-12-24 17:12:23 +0000

    Fixing 'Reactor not Restartable' Error

--- app.py
+++ app.py
@@ -10,32 +10,60 @@ from multiprocessing import Process, Queue
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 
-
 app = Flask(__name__)
 api = Api(app)
 
 class News(Resource):
     def get(self):
-        runner = CrawlerRunner()
-        d = runner.crawl(NewsSpider)
-        d.addBoth(lambda _: reactor.stop())
-        reactor.run()
+        data = []
+        def f(q):
+            try:
+                runner = crawler.CrawlerRunner()
+                deferred = runner.crawl(NewsSpider)
+                deferred.addBoth(lambda _: reactor.stop())
+                reactor.run()
+                q.put(NewsSpider.getNews())
+            except Exception as e:
+                q.put(e)
+
+        q = Queue()
+        p = Process(target=f, args=(q,))
+        p.start()
+        result = q.get()
+        p.join()
+        return {'news': result}
+
+        # runner = CrawlerRunner()
+        # d = runner.crawl(NewsSpider)
+        # d.addBoth(lambda _: reactor.stop())
+        # reactor.run()
         # process = CrawlerProcess({
         #     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
         # })
         # process.crawl(NewsSpider)
         # process.start()
-        return {'news': NewsSpider.getNews() }
+
 
 
 class Galerie(Resource):
     def get(self):
-        process = CrawlerProcess({
-            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-        })
-        process.crawl(GalerieSpider)
-        process.start()
-        return {'galeries': GalerieSpider.getGaleries()}
+        data = []
+        def f(q):
+            try:
+                runner = crawler.CrawlerRunner()
+                deferred = runner.crawl(GalerieSpider)
+                deferred.addBoth(lambda _: reactor.stop())
+                reactor.run()
+                q.put(GalerieSpider.getNews())
+            except Exception as e:
+                q.put(e)
+
+        q = Queue()
+        p = Process(target=f, args=(q,))
+        p.start()
+        result = q.get()
+        p.join()
+        return {'news': result}
 
 
 
--- news.py
+++ news.py
@@ -42,3 +42,4 @@ class NewsSpider(scrapy.Spider):
 
 
 
+
